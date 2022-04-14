e5b35a54e00c5eb487eb2478d3f1ce17cf20eb27
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-14 18:10:01 -0300

    Criado projeto inicial

---
+++ src/main/java/com/github/matskira/pokedex/PokedexApplication.java
@@ -0,0 +1,13 @@
+package com.github.matskira.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApplication.class, args);
+	}
+
+}
